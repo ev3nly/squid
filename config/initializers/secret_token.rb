3da2dee7a780c3d75d853445532a798d032c4e52
Sean Yu <seansu4you87@gmail.com>
2013-09-23 14:30:20 -0700

    First Commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Squid::Application.config.secret_key_base = '6f107c9f7ccb47f760d288eb4e9ef4da4071afc96619ffae85b75ec4d0905e80ba7c374b697a300be0cabb7144b0a83a3fc933c24eb9a17b399905537f599e26'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Squid::Application.config.session_store :cookie_store, key: '_squid_session'
